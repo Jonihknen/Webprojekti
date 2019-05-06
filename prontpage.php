cf9e6248f107c80905770d1ccfa36d5af80ba867
Joni häkkänen <joni.hakkanen@metropolia.fi>
2019-05-06 12:53:47 +0300

    asd

---
+++ prontpage.php
@@ -0,0 +1,74 @@
+<?php include("server.php");
+
+    if (empty($_SESSION["username"])) {
+        header("location: logreg.php");
+    }?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selainpeli X</title>
+    <link rel="stylesheet" type="text/css" href="css/prontpage.css">
+    <script src="//cdn.jsdelivr.net/npm/phaser@3.1.1/dist/phaser.js"></script>
+</head>
+<body>
+    <?php include("game.php");?>
+    <div class="header">
+        <h1> HOME PAGE </h1>
+    </div>
+
+    <div class="content">
+        <?php if (isset($_SESSION["success"])): ?>
+            <div class="error success">
+                <h3>
+                    <?php
+                        echo $_SESSION["success"];
+                        unset($_SESSION["success"]);
+                    ?>
+                </h3>
+            </div>
+        <?php endif ?>
+
+        <?php if (isset($_SESSION["username"])): ?>
+            <p>Welcome <?php echo $_SESSION["username"]; ?></p>
+        <?php endif ?>
+    </div>
+<div id="peli" class="peli"></div>
+    <nav>
+        <ul>
+            <li>Account
+            <ul>
+                <li><a href="game.html">Account settings</a></li>
+                <li><a href="highscore.php?name=<?php echo ($_SESSION['username'])?>">Highscore</a></li>
+                <li><a href="prontpage.php?logout='1'" style="color: red;">logout</a></li>
+            </ul>
+            </li>
+            <li>Game
+                <ul>
+                    <li><a href="game.php">Play the game</a></li>
+                    <li><a href="22.html">Read the rules</a></li>
+                    <li><a href="23.html">Item 2.3</a></li>
+                    <li><a href="24.html">Item 2.4</a></li>
+                </ul>
+            </li>
+            <li><a href="highscore.php">Highscores</a></li>
+        </ul>
+    </nav>
+<p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p>
+    <p>fill</p><p>fill</p>
+    <p>fill</p>
+
+
+</body>
+
+
+</html>
---
+++ server.php
@@ -0,0 +1,161 @@
+<?php
+
+    session_start();
+    $username = "";
+    $errors = array();
+    $db = OpenCon();
+
+function OpenCon(){
+
+    $conn = new mysqli("localhost", "olso", "olso", "webprojekti", 3306) or die("connect failed: %s\n". $conn->error);
+
+    return $conn;
+}
+
+function userexists($username){
+    $db = OpenCon();
+    $sql = $db->prepare("SELECT * FROM users WHERE username=?");
+    $sql->bind_param("s", $username);
+    $sql->execute();
+    $result = $sql->get_result();
+    if (mysqli_num_rows($result) == 1){
+        return true;
+
+    } else{
+        return false;
+    }
+}
+
+    if (isset($_POST["register"])) {
+        $username = strip_tags($_POST["username"]);
+        $password_1 = strip_tags($_POST["password"]);
+        $password_2 = strip_tags($_POST["password_2"]);
+
+        if (empty($username)) {
+            array_push($errors, "Username is required");
+        }
+        if (empty($password_1)) {
+            array_push($errors, "Password is required");
+        }
+
+        if ($password_1 != $password_2) {
+            array_push($errors, "Passwords do not match");
+        }
+        if (userexists($username)){
+            array_push($errors, "username already exists");
+        }
+        if (preg_match("/^[0-9A-Za-z_]+$/", $username) == 0) {
+            array_push($errors, "invalid username");
+        }
+        if (strlen($username) < 3) {
+            array_push($errors, "username must have at least 3 characters");
+        }
+        if (strlen($username) > 10) {
+            array_push($errors, "username too long (maximum is 10 characters)");
+        }
+        if (strlen($password_1) < 8) {
+            array_push($errors, "password must contain at least 8 characters");
+
+        }
+
+        if(count($errors) == 0) {
+            $password_1 = MD5($password_1);
+            $sql = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+            $sql->bind_param("ss", $username,$password_1);
+            $sql->execute();
+            $_SESSION["username"] = $username;
+            $_SESSION["success"] = "You are now logged in";
+            header("location: prontpage.php");
+        }
+
+    }
+    if(isset($_POST["login"])){
+        $username = ($_POST["username"]);
+        $password = MD5($_POST["password"]);
+
+        if (empty($username)) {
+            array_push($errors, "username is required");
+        }
+        if (empty($password)) {
+            array_push($errors, "password is required");
+        }
+
+        if (count($errors) == 0) {
+            //$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+            $sql = $db->prepare("SELECT * FROM users WHERE username=? AND password=?");
+            $sql->bind_param("ss", $username,$password);
+            $sql->execute();
+            $result = $sql->get_result();
+            //$result = mysqli_query($db, $query);
+            if (mysqli_num_rows($result) == 1){
+                $_SESSION["username"] = $username;
+                $_SESSION["success"] = "you are now logged in";
+                header("Location: prontpage.php");
+            }else{
+                array_push($errors, "wrong username or password");
+            }
+        }
+    }
+
+
+    if (isset($_GET["logout"])) {
+        session_destroy();
+        unset($_SESSION["username"]);
+        header("location: logreg.php");
+    }
+    function haeKaikki(){
+        $db = OpenCon();
+        $result = $db->query("SELECT * FROM highscores");
+
+        $outp = "[";
+        while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+            if ($outp != "[") {$outp .= ",";}
+            $outp .= '{"User":"'  . $rs["user"] . '",';
+            $outp .= '"Points":"'   . $rs["points"]        . '"}';
+        }
+        $outp .="]";
+        return ($outp);
+    }
+    function hae($haku){
+        $db = OpenCon();
+        $haku = strip_tags($haku);
+        $query = $db->prepare("SELECT * FROM highscores WHERE user=?");
+        $query->bind_param("s", $haku);
+        $query->execute();
+        $result = $query->get_result();
+
+        $outp = "[";
+        while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+            if ($outp != "[") {$outp .= ",";}
+            $outp .= '{"User":"'  . $rs["user"] . '",';
+            $outp .= '"Points":"'   . $rs["points"]        . '"}';
+        }
+        $outp .="]";
+        $query->close();
+        return ($outp);
+    }
+    function muuta($pisteet, $haku){
+        $db = OpenCon();
+        $haku = strip_tags($haku);
+        $query = $db->prepare("UPDATE highscores SET points =? WHERE user =?");
+        $query->bind_param("ss", $pisteet, $haku);
+        $query->execute();
+        $result = $query->get_result();
+
+        $outp = "[";
+        while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+            if ($outp != "[") {$outp .= ",";}
+            $outp .= '{"User":"'  . $rs["user"] . '",';
+            $outp .= '"Points":"'   . $rs["points"]        . '"}';
+        }
+        $outp .="]";
+        $query->close();
+        return ($outp);
+    }
+    function tee($pisteet, $haku){//Tänne pitää viel tehä tarkastus et onko se jo tietokannassa
+        $db = OpenCon();
+        $query = $db->prepare("INSERT INTO highscores VALUES (?,?)");
+        $query->bind_param("ss",  $haku, $pisteet);
+        $query->execute();
+        $query->close();
+    }
