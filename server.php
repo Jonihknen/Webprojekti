b07b70f596dd0c7c9108b4653949a500d0d6b50e
Joni häkkänen <joni.hakkanen@metropolia.fi>
2019-05-06 11:19:27 +0300

    serveriä muokattu

--- server.php
+++ server.php
@@ -12,6 +12,20 @@ function OpenCon(){
     return $conn;
 }
 
+function userexists($username){
+    $db = OpenCon();
+    $sql = $db->prepare("SELECT * FROM users WHERE username=?");
+    $sql->bind_param("s", $username);
+    $sql->execute();
+    $result = $sql->get_result();
+    if (mysqli_num_rows($result) == 1){
+        return true;
+
+    } else{
+        return false;
+    }
+}
+
     if (isset($_POST["register"])) {
         $username = strip_tags($_POST["username"]);
         $password_1 = strip_tags($_POST["password"]);
@@ -27,6 +41,23 @@ function OpenCon(){
         if ($password_1 != $password_2) {
             array_push($errors, "Passwords do not match");
         }
+        if (userexists($username)){
+            array_push($errors, "username already exists");
+        }
+        if (preg_match("/^[0-9A-Za-z_]+$/", $username) == 0) {
+            array_push($errors, "invalid username");
+        }
+        if (strlen($username) < 3) {
+            array_push($errors, "username must have at least 3 characters");
+        }
+        if (strlen($username) > 10) {
+            array_push($errors, "username too long (maximum is 10 characters)");
+        }
+        if (strlen($password_1) < 8) {
+            array_push($errors, "password must contain at least 8 characters");
+
+        }
+
         if(count($errors) == 0) {
             $sql = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
             $sql->bind_param("ss", $username,$password_1);
@@ -35,6 +66,7 @@ function OpenCon(){
             $_SESSION["success"] = "You are now logged in";
             header("location: prontpage.php");
         }
+
     }
     if(isset($_POST["login"])){
         $username = strip_tags($_POST["username"]);
