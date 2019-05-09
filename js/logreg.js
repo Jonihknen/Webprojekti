9e717f8bdf984773b2974d9c0cd7b122ce5aabfe
Joni häkkänen <joni.hakkanen@metropolia.fi>
2019-05-09 17:05:09 +0300

    dd

--- js/logreg.js
+++ js/logreg.js
@@ -11,23 +11,40 @@ if (window.location.hash === '#register') {
 
 document.getElementById('regclick').onclick = function() {
 	var element = document.getElementsByClassName('error');
+	var loginname = document.getElementsByName('username');
+	var password = document.getElementsByName('password');
 	document.getElementById('login').style.display = 'none';
 	if(element[0] || element[1]){
 		element[0].style.display = 'none';
 		element[1].style.display = 'none';
 	}
+	if(loginname[1] || password[1]){
+		loginname[1].value = '';
+		password[1].value = '';
+	}
+
+
 	document.getElementById('register').style.display = 'block';
 
 };
 
 document.getElementById('logclick').onclick = function() {
 	var element = document.getElementsByClassName('error');
+	var loginname = document.getElementsByName('username');
+	var password = document.getElementsByName('password');
 	if(element[0] || element[1]){
 		element[1].style.display = 'none';
 		element[0].style.display = 'none';
 	}
+	if(loginname[1] || password[1]){
+
+		loginname[1].value = '';
+		password[1].value = '';
+	}
+
 	document.getElementById('login').style.display = 'block';
 	document.getElementById('register').style.display = 'none';
+
 };
 
 
